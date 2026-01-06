d5f4aeb4fb77cd2d81893898b9aae5e274df445c
bhanuka prabhath <bhanukaprabhath28@gmail.com>
2026-01-06 08:46:58 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/Auth/Enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.Auth.Enums;
-
-public enum Role {
-    USER,ADMIN
-}
--- backend/src/main/java/com/example/backend/order/Controller/OrderController.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.order.Controller;
-
-public class OrderController {
-
-}
