fa561cbe445257a67e3342c98cce0a4bf86ef8e1
raniduanjula <raniduanjula205@gmail.com>
2026-02-16 16:40:23 +0530

    Delete Ecommerce directory

--- Ecommerce/frontend/src/api/categoryApi.ts
+++
@@ -1,9 +0,0 @@
-import api from './axiosConfig';
-import { Category } from '../types/product.types';
-
-export const categoryApi = {
-    getAllCategories: async (): Promise<Category[]> => {
-        const response = await api.get('/api/categories');
-        return response.data;
-    }
-};
--- Ecommerce/frontend/src/api/userApi.ts
+++
@@ -1,9 +0,0 @@
-import api from './axiosConfig';
-import { User } from '../types/auth.types';
-
-export const userApi = {
-    getAllUsers: async (): Promise<User[]> => {
-        const response = await api.get('/api/admin/users');
-        return response.data;
-    },
-};
